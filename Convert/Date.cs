54483d46fc34e16b36606cfe0265369533f8d74e
dark fox <danyamatlaga63@gmail.com>
2022-12-27 19:48:20 +0200

    MultiBilding

---
+++ Convert/Date.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Convert
+{
+    class Date
+    {
+
+        private int year;
+        private int month;
+        private int day;
+
+        public int Year
+        {
+            get { return year; }
+            set { SetProperty(ref year, value, nameof(Year)); }
+        }
+        public int Month
+        {
+            get { return month; }
+            set { SetProperty(ref month, value, nameof(Month)); }
+        }
+        public int Day
+        {
+            get { return day; }
+            set { SetProperty(ref day, value, nameof(Day)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetProperty<T>(ref T field, T value, string name)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+    public class YearRules : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int year;
+            if (!int.TryParse(value.ToString(), out year))
+            {
+                return new ValidationResult(false, "false parse");
+            }
+
+            if (year > 9999 || year < 1)
+            {
+                return new ValidationResult(false, "invalid year");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+
+    public class MonthRules : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int month;
+            if (!int.TryParse(value.ToString(), out month))
+            {
+                return new ValidationResult(false, "false parse");
+            }
+
+            if (month > 12 || month < 1)
+            {
+                return new ValidationResult(false, "invalid month");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+
+    public class DayRules : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int day;
+            if (!int.TryParse(value.ToString(), out day))
+            {
+                return new ValidationResult(false, "false parse");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
---
+++ Convert/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Convert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+    class ConvertIntToDate : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date;
+
+            try
+            {
+                date = new DateTime(System.Convert.ToInt32((values[0].ToString())), System.Convert.ToInt32((values[1].ToString())), System.Convert.ToInt32((values[2].ToString())));
+            }
+            catch (Exception)
+            {
+                return DateTime.MinValue;
+            }
+
+            return date;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+            object[] intArr = new object[3];
+
+            intArr[0] = date.Year;
+            intArr[1] = date.Month;
+            intArr[2] = date.Day;
+
+            return intArr;
+        }
+    }
+}
