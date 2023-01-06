3019678c418d13569388b1902f0cb0e539e21aa2
dark fox <danyamatlaga63@gmail.com>
2023-01-06 16:46:34 +0200

    Contact_List

--- Authentication/MainWindow.xaml.cs
+++ Authentication/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Authentication
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
